d6bacf56018e777500554d5e62860b8a7ccc9969
Chritzel <16166683+Chritzel@users.noreply.github.com>
2021-12-22 22:52:56 +0100

    Semseg version

--- scripts/screen_detector/main.py
+++ scripts/screen_detector/main.py
@@ -1,5 +1,6 @@
 # Import TF and TF Hub libraries.
 import tensorflow as tf
+from tensorflow._api.v2 import data
 from tensorflow.python.util.dispatch import _signature_from_annotations
 import tensorflow_addons as tfa
 from filters import gaussian_filter2d
@@ -18,18 +19,21 @@ import argparse
 # 576x384 (wxh)
 # Input shape: hxw
 def create_model(input_shape=(384,576,3)) -> Model:
-    input = layers.Input(shape=input_shape)
-    input = mobilenet_v2.preprocess_input(input)
-    backbone = mobilenet_v2.MobileNetV2(input_tensor=input, include_top=False, weights='imagenet')
+    i = layers.Input(shape=input_shape)
+    x = tf.cast(i, tf.float32)
+    x = mobilenet_v2.preprocess_input(x)
+
+    backbone = mobilenet_v2.MobileNetV2(input_tensor=x, include_top=False, weights='imagenet')
     backbone.trainable = False
-    x = backbone.output
+    x = backbone(x)
 
     # Get layers and attach FPN
     backbone_scale_2 = backbone.get_layer('expanded_conv_project_BN').output
     backbone_scale_3 = backbone.get_layer('block_2_add').output
     backbone_scale_4 = backbone.get_layer('block_5_add').output
     backbone_scale_5 = backbone.get_layer('block_12_add').output
-    backbone_scale_6 = backbone.get_layer('block_16_project_BN').output
+    backbone_scale_6 = x
+    # backbone_scale_6 = backbone.get_layer('block_16_project_BN').output
 
     # FPN
     fpn_d = 128
@@ -39,42 +43,60 @@ def create_model(input_shape=(384,576,3)) -> Model:
 
     m6 = layers.Conv2D(fpn_d, 1, padding='same', name='m6')(backbone_scale_6)
     p6 = layers.Conv2D(fpn_d, 3, padding='same', name='p6')(m6)
+    p6 = layers.Conv2D(fpn_d, 3, padding='same')(p6)
+    # p6 = layers.Conv2D(fpn_d, 3, padding='same')(p6)
     p6_up = layers.UpSampling2D(size=(16,16))(p6)
 
     y = layers.Conv2D(fpn_d, 1, padding='same')(backbone_scale_5)
     z = layers.UpSampling2D()(m6)
     m5 = layers.Add(name='m5', )([y,z])
     p5 = layers.Conv2D(fpn_d, 3, padding='same', name='p5')(m5)
+    p5 = layers.Conv2D(fpn_d, 3, padding='same')(p5)
+    # p5 = layers.Conv2D(fpn_d, 3, padding='same')(p5)
     p5_up = layers.UpSampling2D(size=(8,8))(p5)
 
     y = layers.Conv2D(fpn_d, 1, padding='same')(backbone_scale_4)
     z = layers.UpSampling2D()(m5)
     m4 = layers.Add(name='m4', )([y,z])
     p4 = layers.Conv2D(fpn_d, 3, padding='same', name='p4')(m4)
+    p4 = layers.Conv2D(fpn_d, 3, padding='same')(p4)
+    # p4 = layers.Conv2D(fpn_d, 3, padding='same')(p4)
     p4_up = layers.UpSampling2D(size=(4,4))(p4)
 
     y = layers.Conv2D(fpn_d, 1, padding='same')(backbone_scale_3)
     z = layers.UpSampling2D()(m4)
     m3 = layers.Add(name='m3', )([y,z])
     p3 = layers.Conv2D(fpn_d, 3, padding='same', name='p3')(m3)
+    p3 = layers.Conv2D(fpn_d, 3, padding='same')(p3)
+    # p3 = layers.Conv2D(fpn_d, 3, padding='same')(p3)
     p3_up = layers.UpSampling2D()(p3)
 
     y = layers.Conv2D(fpn_d, 1, padding='same')(backbone_scale_2)
     z = layers.UpSampling2D()(m3)
     m2 = layers.Add(name='m2', )([y,z])
     p2 = layers.Conv2D(fpn_d, 3, padding='same', name='p2')(m2)
+    p2 = layers.Conv2D(fpn_d, 3, padding='same')(p2)
+    # p2 = layers.Conv2D(fpn_d, 3, padding='same')(p2)
 
     # Concatenate Outputs of FPN
+    # x = layers.Concatenate()([p3, p4_up, p5_up, p6_up])
     x = layers.Concatenate()([p2, p3_up, p4_up, p5_up, p6_up])
     x = layers.Dropout(0.1)(x)
 
     # Boil it down to four heatmaps
-    x = layers.Conv2D(32, 3, padding='same')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Conv2D(4, 1, activation='relu', padding='same')(x)
-    x = layers.UpSampling2D()(x)
-    model = Model(inputs=backbone.input, outputs=x)
-    return model
+    x = layers.Conv2D(fpn_d, 3, padding='same')(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Conv2D(fpn_d, 3, padding='same')(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Conv2D(fpn_d, 3, padding='same')(x)
+    # x = layers.Conv2D(fpn_d, 3, padding='same', activation='relu')(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.Conv2D(1, 1, padding='same')(x)
+    # x = layers.Conv2D(5, 1, padding='same')(x)
+    # x = layers.UpSampling2D(interpolation='bilinear', size=(4,4))(x)
+
+    model = Model(inputs=i, outputs=x)
+    return model, backbone
 
 def get_corner_points(polygon: np.ndarray) -> List:
     # Ordering: TL TR BL BR
@@ -114,7 +136,7 @@ def get_corner_points(polygon: np.ndarray) -> List:
 
     return corners.tolist()
 
-def create_coco_dataset(set: str, min_area_percentage: float = 0.0):
+def create_coco_dataset(set: str, min_area_percentage: float = 0.05):
     """Returns a tf dataset with (image_file_name, corners)
 
     Args:
@@ -213,11 +235,16 @@ def get_padding_params(height, width, target_height, target_width):
 
     return resized_height, resized_width, p_height, p_width
 
+def draw_poly(image, poly):
+    img = image.numpy()
+    cv2.fillConvexPoly(img, poly.numpy(), (1), cv2.LINE_AA)
+    return img
+
 def make_loading_and_heatmap_generation_function(target_width, target_height, sigma=tf.Variable):
     def load_img_and_make_heatmaps(filename, corners):
         image = tf.io.read_file(filename)
-        image = tf.io.decode_jpeg(image)
-        image = tf.image.convert_image_dtype(image, tf.float32)
+        image = tf.io.decode_jpeg(image, channels=3)
+        image = tf.image.convert_image_dtype(image, tf.uint8)
 
         image_shape = tf.shape(image)
 
@@ -254,7 +281,21 @@ def make_loading_and_heatmap_generation_function(target_width, target_height, si
         heatmaps = tf.map_fn(create_heatmap, corners, fn_output_signature=tf.TensorSpec(shape=(target_height, target_width), dtype=tf.float32))
 
         heatmaps = tf.stack(tf.unstack(heatmaps, axis=0), axis=2)
+        
+        # Make a polygon
+        corners_reorded = tf.gather(corners, [0,1,3,2])
+        corners_reorded = tf.reverse(corners_reorded, axis=[1])
+        poly = np.zeros((target_height, target_width), np.float32)
+        [poly,] = tf.py_function(draw_poly, [poly, corners_reorded],[tf.float32])
+
+        poly = tf.convert_to_tensor(poly)
+        poly = tf.expand_dims(poly, axis=2)
+        heatmaps = tf.concat([heatmaps, poly], axis=2)
+
         heatmaps = heatmaps / tf.reduce_max(heatmaps)
+        heatmaps = tf.image.resize(heatmaps, (target_height // 2, target_width // 2))
+
+        heatmaps = tf.gather(heatmaps, [4], axis=2)
 
         image = tf.image.resize_with_pad(image, target_height, target_width)
 
@@ -282,22 +323,25 @@ def show(image, heatmaps):
     if isinstance(image, tf.Tensor):
         image = image.numpy()
 
+    image = image / 255.0
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     if isinstance(heatmaps, tf.Tensor):
         heatmaps = heatmaps.numpy()
 
-    heatmaps = [heatmaps[:,:,0], heatmaps[:,:,1], heatmaps[:,:,2], heatmaps[:,:,3]]
+    # heatmaps = [heatmaps[:,:,0], heatmaps[:,:,1], heatmaps[:,:,2], heatmaps[:,:,3], heatmaps[:,:,4]]
+    heatmaps = [heatmaps[:,:,0]]
 
-    colors = [(0,0,1), (1,0,0), (0,1,1), (0,1,0)]
+    colors = [(0,0,1), (1,0,0), (0,1,1), (0,1,0), (0,0,1)]
     # TL = Red, TR = Blue, BL = Yellow, BR = Green
 
     heatmaps_combined = np.zeros(image.shape)
     for i, heatmap in enumerate(heatmaps):
         heatmap = cv2.cvtColor(heatmap, cv2.COLOR_GRAY2BGR)
         heatmap *= np.array(colors[i])
+        heatmap = cv2.resize(heatmap, (image.shape[1], image.shape[0]))
         heatmaps_combined += heatmap
-
+    
     image = 1 - (1-image) * (1-heatmaps_combined)
     cv2.imshow('Image', image)
     cv2.waitKey(0)
@@ -317,7 +361,8 @@ def create_heatmap_dataset(files_dataset, image_width, image_height, sigma):
     load_img_and_make_heatmaps = make_loading_and_heatmap_generation_function(image_width, image_height, sigma)
     files_dataset = files_dataset.shuffle(len(files_dataset))
     dataset = files_dataset.map(load_img_and_make_heatmaps, num_parallel_calls=tf.data.AUTOTUNE)
-    dataset = dataset.batch(batch_size).repeat()
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.repeat()
     dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
     return dataset
@@ -328,8 +373,21 @@ class AdjustSigmaCallback(tf.keras.callbacks.Callback):
         self.sigma = sigma
 
     def on_epoch_end(self, epoch=None, logs=None):
-        self.sigma.assign(self.sigma - 1 if self.sigma > 3 else self.sigma)
-        tf.print("Sigma: ", self.sigma)
+        if epoch >= 14 and epoch % 4 == 0:
+            self.sigma.assign(self.sigma - 2 if self.sigma > 7 else self.sigma)
+        
+        tf.print(" - Sigma: ", self.sigma)
+
+class MakeBackboneTrainableCallback(tf.keras.callbacks.Callback):
+    def __init__(self, backbone: tf.keras.Model):
+        super(MakeBackboneTrainableCallback, self).__init__()
+        self.backbone = backbone
+    
+    def on_epoch_end(self, epoch=None, logs=None):
+        if epoch == 14:
+            self.backbone.trainable = True
+            print(" - Making backbone trainable.")
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -340,7 +398,7 @@ if __name__ == "__main__":
     image_size = (288,192)
     image_width, image_height = image_size
 
-    sigma = tf.Variable(14)
+    sigma = tf.Variable(20)
 
     adjust_sigma_cb = AdjustSigmaCallback(sigma)
 
@@ -382,21 +440,23 @@ if __name__ == "__main__":
         model = tf.keras.models.load_model(args.load_model_path)
     else:
         print("Creating model.")
-        model = create_model(input_shape=(image_height, image_width, 3))
+        model, backbone = create_model(input_shape=(image_height, image_width, 3))
+        make_backbone_trainable_cb = MakeBackboneTrainableCallback(backbone)
 
     model.summary()
-    model.compile(optimizer=optimizers.Adam(learning_rate=0.00001, clipnorm=1), loss=losses.MeanSquaredError())
+    model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss=losses.MeanSquaredError())
 
     if args.train:
 
-        subpath = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        # subpath = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        subpath = 'baseline'
 
         log_dir = "logs/fit/" + subpath
         tensorboard_callback = tf.keras.callbacks.TensorBoard(
             log_dir=log_dir,
             histogram_freq=1,
             update_freq='batch',
-            profile_batch=(10,20)
+            # profile_batch=(10,20)
             )
 
         checkpoint_path = "checkpoints/"+subpath+"/checkpoint"
@@ -414,21 +474,27 @@ if __name__ == "__main__":
             patience=2, 
             verbose=1,
             mode='min',
-            min_lr = 0.000001)
+            min_lr = 0.0000001)
 
         history = model.fit(
             x=train_dataset,
-            epochs=30,
-            steps_per_epoch=100,
+            epochs=50,
+            steps_per_epoch=len(train_files_dataset) // batch_size,
             validation_data=val_dataset,
-            validation_steps=5,
-            callbacks=[tensorboard_callback, model_checkpoint_callback, lr_callback, adjust_sigma_cb] # adjust_sigma_cb
+            validation_steps=len(val_files_dataset) // batch_size,
+            callbacks=[tensorboard_callback, lr_callback, make_backbone_trainable_cb, adjust_sigma_cb]
+            # callbacks=[tensorboard_callback, model_checkpoint_callback, lr_callback]
+            # callbacks=[tensorboard_callback, model_checkpoint_callback, lr_callback, adjust_sigma_cb]
             )
     
+        model.save(checkpoint_path)
+
     if args.eval_qual:
         val_dataset = val_dataset.unbatch()
         for image, heatmaps in val_dataset.take(100):
             exp_image = tf.expand_dims(image, axis=0)
-            pred_heatmaps = model.predict(exp_image)
+            pred_heatmaps = model(exp_image)
+            # pred_heatmaps = model.predict(exp_image)
             pred_heatmaps = tf.squeeze(pred_heatmaps, axis=0)
+            pred_heatmaps = pred_heatmaps / tf.reduce_max(pred_heatmaps, axis=[0,1,2])
             show(image, pred_heatmaps)
